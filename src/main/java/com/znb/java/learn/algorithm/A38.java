225327a1bc07a43ebf4e7775329c4a5997d34412
张乃斌 <zhangnaibin@xiaomi.com>
2016-05-20 18:46:06 +0800

    add

---
+++ src/main/java/com/znb/java/learn/algorithm/A38.java
@@ -0,0 +1,13 @@
+package com.znb.java.learn.algorithm;
+
+/**
+ * 输入数字n，按顺序输出从1最大的n位10进制数。比如输入3，则输出1、2、3一直到最大的3位数即999
+ *
+ * see http://zhedahht.blog.163.com/blog/static/2541117420094279426862/
+ *
+ * 字符串操作
+ * @author zhangnaibin@xiaomi.com
+ * @time 2016-05-19 下午7:54
+ */
+public class A38 {
+}
