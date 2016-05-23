27fb89e26ed090ff6f77961f3f7aa31a85eefb1d
张乃斌 <zhangnaibin@xiaomi.com>
2016-05-23 11:42:45 +0800

    add leetcode 2

--- src/main/java/com/znb/java/learn/leecode/P2.java
+++ src/main/java/com/znb/java/learn/leecode/P2.java
@@ -26,10 +26,8 @@ public class P2 {
 
         ListNode result = l1;
         boolean mark = false;
-        ListNode pre = null;
-        ListNode temp = l1;
+        ListNode temp = null;
         while (l1 != null && l2 != null) {
-            pre = l1;
             int val;
             if (mark) {
                 val = l1.val + l2.val + 1;
@@ -42,14 +40,144 @@ public class P2 {
                 mark = true;
             }
             l1.val = val;
+            temp = l1;
             l1 = l1.next;
             l2 = l2.next;
         }
 
         while (l1 != null) {
+            int val;
+            if (mark) {
+                val = l1.val + 1;
+                mark = false;
+            } else {
+                val = l1.val;
+            }
+            if (val >= 10) {
+                val = val % 10;
+                mark = true;
+            }
+            l1.val = val;
+            temp = l1;
+            l1 = l1.next;
+        }
 
+        if (l2 != null) {
+            temp.next = l2;
+        }
+        while (l2 != null) {
+            int val;
+            if (mark) {
+                val = l2.val + 1;
+                mark = false;
+            } else {
+                val = l2.val;
+            }
+            if (val >= 10) {
+                val = val % 10;
+                mark = true;
+            }
+            l2.val = val;
+            temp = l2;
+            l2 = l2.next;
+        }
+
+        if (l1 == null && l2 == null && mark) {
+            ListNode node = new ListNode(1);
+            node.next = null;
+            temp.next = node;
         }
 
         return result;
+    }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode result = l1;
+        boolean mark = false;
+        ListNode temp = null;
+        while (l1.next != null && l2.next != null) {
+            int val;
+            if (mark) {
+                val = l1.val + l2.val + 1;
+                mark = false;
+            } else {
+                val = l1.val + l2.val;
+            }
+            if (val >= 10) {
+                val = val % 10;
+                mark = true;
+            }
+            l1.val = val;
+            temp = l1;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1.next != null) {
+            int val;
+            if (mark) {
+                val = l1.val + 1;
+                mark = false;
+            } else {
+                val = l1.val;
+            }
+            if (val >= 10) {
+                val = val % 10;
+                mark = true;
+            }
+            l1.val = val;
+            temp = l1;
+            l1 = l1.next;
+        }
+
+        if (l2.next != null) {
+            temp.next = l2;
+        }
+        while (l2 != null) {
+            int val;
+            if (mark) {
+                val = l2.val + 1;
+                mark = false;
+            } else {
+                val = l2.val;
+            }
+            if (val >= 10) {
+                val = val % 10;
+                mark = true;
+            }
+            l2.val = val;
+            temp = l2;
+            l2 = l2.next;
+        }
+
+        if (l1 == null && l2 == null && mark) {
+            ListNode node = new ListNode(1);
+            node.next = null;
+            temp.next = node;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(9);
+        ListNode node3 = new ListNode(9);
+        node2.next = node3;
+        ListNode result = new P2().addTwoNumbers(node1, node2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
     }
 }
---
+++ src/main/java/com/znb/java/learn/leecode/P3.java
@@ -0,0 +1,11 @@
+package com.znb.java.learn.leecode;
+
+/**
+ * @author zhangnaibin@xiaomi.com
+ * @time 2016-05-23 上午11:34
+ */
+public class P3 {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
